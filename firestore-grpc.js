55b6893e778da4b8f29f9694c007093c501e5619
Travis Fischer <fisch0920@gmail.com>
2020-03-27 16:37:36 -0400

    bugfix from google team! 💯

--- firestore-grpc.js
+++ firestore-grpc.js
@@ -1,7 +1,8 @@
 const { Firestore } = require('@google-cloud/firestore')
+const grpc = require('@grpc/grpc-js')
 
 console.log('using gRPC via @google-cloud/firestore')
-const db = new Firestore()
+const db = new Firestore({ grpc })
 const CronJobs = db.collection('cron-jobs')
 
 exports.get = async function get(id) {
